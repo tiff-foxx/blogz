4d39e8dde2173a2edae5689d486f6157903f8e68
Tiffany Foxx <tiff.foxx@gmail.com>
2018-11-03 18:43:47 -0400

    Complete buid-a-blog

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:aplus@localhost:8889/build-a-blog'
@@ -34,15 +35,20 @@ def index():
 
 @app.route('/newpost', methods = ['GET','POST'])
 def new_post():
+    
     if request.method == 'POST':
         blog_title = request.form['title']
         blog_body = request.form['body']
-        empty_error = ''
-        if len(blog_title)<= 0 or len(blog_body)<=0:
-            empty_error = 'Title and Body fields required'
-            return render_template('newpost.html',title='TESTING',empty_error=empty_error,blog_title=blog_title,blog_body=blog_body)
-            
-        else: 
+        title_error = ''
+        body_error = ''
+
+        if len(blog_title) <= 0:
+            title_error = 'Please enter title'
+
+        if len(blog_body) <= 0:
+            body_error = 'Please enter body'
+
+        if not title_error and not body_error:
             new_entry = Blog(blog_title, blog_body)
             db.session.add(new_entry)
             db.session.commit()
@@ -50,8 +56,25 @@ def new_post():
             # return render_template('blog.html',title="Build-A-Blog",entries=entries)
             entries = Blog.query.filter_by(id=new_entry.id).all()
             return render_template('individual.html',title='latest_entry',entries=entries)
+        else:
+            return render_template('newpost.html',title='TESTING',title_error=title_error,body_error=body_error,blog_title=blog_title,blog_body=blog_body)
 
     return render_template('newpost.html',title="New Post")
+
+
+        # if len(blog_title)<= 0 or len(blog_body)<=0:
+        #     empty_error = 'Title and Body fields required'
+        #     return render_template('newpost.html',title='TESTING',empty_error=empty_error,blog_title=blog_title,blog_body=blog_body)
+            
+        # else: 
+        #     new_entry = Blog(blog_title, blog_body)
+        #     db.session.add(new_entry)
+        #     db.session.commit()
+        #     # entries = Blog.query.filter_by(submitted=True).all()
+        #     # return render_template('blog.html',title="Build-A-Blog",entries=entries)
+        #     entries = Blog.query.filter_by(id=new_entry.id).all()
+        #     return render_template('individual.html',title='latest_entry',entries=entries)
+
 
 @app.route("/individual")
 def individual_blog():
