5eed0ec0a79734589c8002a39baf0f68d25df763
Tiffany Foxx <tiff.foxx@gmail.com>
2018-11-03 15:40:55 -0400

    still need to get error msg to work and show individual page to finish through use case 1

--- main.py
+++ main.py
@@ -28,34 +28,11 @@ def index():
         blog_title = request.form['title']
         blog_body = request.form['body']
         new_entry = Blog(blog_title, blog_body)
-        # db.session.add(new_entry)
-        # db.session.commit()
-        
-
+      
     entries = Blog.query.filter_by(submitted=True).all()
     return render_template('blog.html',title="Build-A-Blog",entries=entries)
 
-
-# @app.route('/', methods=['POST', 'GET'])
-# def index():
-
-#     if request.method == 'POST':
-#         task_name = request.form['task']
-#         new_task = Task(task_name)
-#         db.session.add(new_task)
-#         db.session.commit()
-
-#     tasks = Task.query.filter_by(completed=False).all()
-#     completed_tasks = Task.query.filter_by(completed=True).all()
-#     return render_template('todos.html',title="Get It Done!", 
-#         tasks=tasks, completed_tasks=completed_tasks)
-
 @app.route('/newpost', methods = ['GET','POST'])
-
-
-
-
-
 def new_post():
     if request.method == 'POST':
         blog_title = request.form['title']
@@ -73,25 +50,19 @@ def new_post():
             return render_template('blog.html',title="Build-A-Blog",entries=entries)
 
     return render_template('newpost.html',title="New Post")
+
+@app.route("/individual")
+def individual_blog():
+    request.args.get('id')
+    request.args.get('blog_body')
+    return render_template('individual.html',title='123TESTING')
+    
+    #username = request.form['username']
+    #template = jinja_env.get_template('valid_signup.html')
+    #return template.render(name = username) -->
+
     # entries = Blog.query.filter_by(submitted=True).all()
     # return render_template('blog.html',title="Build-A-Blog")
-
-
-
-
-#    entries = Blog.query.filter_by(submitted=True).all()
-#    return render_template('main_blog_pg.html',title="Build-A-Blog")
-
-# @app.route('/delete-task', methods=['POST'])
-# def delete_task():
-
-#     task_id = int(request.form['task-id'])
-#     task = Task.query.get(task_id)
-#     task.completed = True
-#     db.session.add(task)
-#     db.session.commit()
-
-#     return redirect('/')
 
 if __name__ == '__main__':
     app.run()
