64ab1edf5d1c065096dc5033ca129303d6015929
Tiffany Foxx <tiff.foxx@gmail.com>
2018-11-04 21:39:33 -0500

    signroute page is working

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:Done530@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-# app.secret_key = '1234zyxw'
+app.secret_key = '1234zyxw'
 
 
 class Blog(db.Model):
@@ -32,10 +32,8 @@ class User(db.Model):
     blogs = db.relationship('Blog', backref='owner')
 
     def __init__(self, username, password):
-        self.email = email
         self.username = username
         self.password = password
-
 
 
 @app.route('/blog', methods=['GET','POST'])
@@ -48,6 +46,7 @@ def index():
       
     entries = Blog.query.filter_by(submitted=True).all()
     return render_template('blog.html',title="Build-A-Blog",entries=entries)
+
 
 @app.route('/newpost', methods = ['GET','POST'])
 def new_post():
@@ -65,9 +64,11 @@ def new_post():
             body_error = 'Please enter body'
 
         if not title_error and not body_error:
-            new_entry = Blog(blog_title, blog_body, owner)
+            new_entry = Blog(blog_title, blog_body)
             db.session.add(new_entry)
             db.session.commit()
+            # entries = Blog.query.filter_by(submitted=True).all()
+            # return render_template('blog.html',title="Build-A-Blog",entries=entries)
             entries = Blog.query.filter_by(id=new_entry.id).all()
             return render_template('individual.html',title='latest_entry',entries=entries)
         else:
@@ -76,12 +77,101 @@ def new_post():
     return render_template('newpost.html',title="New Post")
 
 
-
 @app.route("/individual")
 def individual_blog():
     blog_id = request.args.get('id')
     entries = Blog.query.filter_by(id=blog_id).all()
     return render_template('individual.html',title='123TESTING',entries=entries)
+
+
+
+
+
+
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        
+        if user and user.password == password:
+            session['username'] = username
+            flash('Logged in')
+            return redirect ('/newpost')
+
+        if user.password != password:
+            flash('Password incorrect')
+            return redirect ('/login')
+
+        if user.username != username:
+            flash('Username does not exist')
+            return redirect ('/login')
+
+    return render_template('login.html',title="Login Page")
+    
+# def is_blank(variable):
+#     try:
+#         len(variable) == 0
+#         return True
+#     except:
+#         return False
+
+
+@app.route('/signup', methods = ['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+        
+        blank_error = ''
+        username_error = ''
+        length_error = ''
+        verify_error = ''
+   
+        if len(username) == 0 or len(password) == 0 or len(verify) == 0:
+            blank_error = 'One or more fields empty'
+            return render_template('signup.html',title="Try AgainA",
+            username=username,password=password,verify=verify,blank_error=blank_error,
+            username_error=username_error,length_error=length_error,verify_error=verify_error)
+        
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user: 
+            username_error = 'Username already exists'
+            return render_template('signup.html',title="Try AgainD",username=username,password=password,
+            verify=verify,blank_error=blank_error, username_error=username_error,length_error=length_error,
+            verify_error=verify_error)
+
+        if password != verify:
+            verify_error = 'Passwords must match'
+            return render_template('signup.html',title="Try AgainB",
+            username=username,password=password,verify=verify,blank_error=blank_error,
+            username_error=username_error,length_error=length_error,verify_error=verify_error)
+
+        if len(username) <3 or len(password) <3:
+            length_error = 'Username and password must be between 3-20 characters'
+            return render_template('signup.html',title="Try AgainC",username=username,
+            password=password,verify=verify,blank_error=blank_error,
+            username_error=username_error,length_error=length_error,verify_error=verify_error)
+      
+        if not blank_error and not username_error and not length_error and not verify_error:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            flash('Welcome')
+            return redirect ('/newpost')
+        # else:
+        #     return render_template('signup.html',title="Try Again")
+    else:
+        return render_template('signup.html',title="Blogz Signup")
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
